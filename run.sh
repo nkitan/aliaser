6af8288792474f47efdb1860d82cb0bb95ae8274
Ankit Das <ankitdas2k@gmail.com>
2020-07-19 20:27:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@ python3 aliaser.py
  if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]
  then                         
      source $HOME/.zshrc 
-     
-    
+       
  elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]
  then
       source $HOME1/.bashrc
